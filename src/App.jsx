227a56a3f800779e07dc3c02791ca1ba26c20cd2
sibasish005 <sibasishchakraborti@gmail.com>
2025-12-03 22:23:24 +0530

    updated

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1>learn react with chai aur code</h1>
+     <p>this is a sub string</p>
+    </>
+    
+  )
+}
+
+export default App
